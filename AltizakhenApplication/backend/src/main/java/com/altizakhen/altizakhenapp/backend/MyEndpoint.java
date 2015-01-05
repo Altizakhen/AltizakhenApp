674c664df7594db3a8cdf9eeb3e3d009210c4b95
ManarShehade <manar.24@hotmail.com>
2015-01-05 19:42:33 +0200

    Revert "Added Datastore to our endpoints"
    This reverts commit a89e8d46b3801d89bb5be5fa1363318935fa203b.

--- AltizakhenApplication/backend/src/main/java/com/altizakhen/altizakhenapp/backend/MyEndpoint.java
+++ AltizakhenApplication/backend/src/main/java/com/altizakhen/altizakhenapp/backend/MyEndpoint.java
@@ -9,16 +9,9 @@ package com.altizakhen.altizakhenapp.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Named;
 
@@ -34,45 +27,19 @@ public class MyEndpoint {
     }};
 
     @ApiMethod(name = "addItem")
-    public void addItem(@Named("Name") String name, @Named("Location") String location,
-                        @Named("Price") int price,  @Named("SellerId") int sellerId,
-                        @Named("SellerName") String sellerName,
-                        @Named("Description") String description) {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    public Item addItem(@Named("name") String name, @Named("location") String location,
+                        @Named("price") int price,  @Named("sellerId") int sellerId,
+                        @Named("sellerName") String sellerName,
+                        @Named("description") String description) {
+        Item item = new Item(name, location, price, sellerId, sellerName, description);
+        items.add(item);
 
-        Entity item = new Entity("Item");
-        item.setProperty("Name", name);
-        item.setProperty("Location", location);
-        item.setProperty("Price", price);
-        item.setProperty("SellerId", sellerId);
-        item.setProperty("SellerName", sellerName);
-        item.setProperty("Description", description);
-
-        datastore.put(item);
+        return item;
     }
 
     @ApiMethod(name = "getAllItems")
     public List<Item> getAllItems() {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-        Query query = new Query("Item");
-        PreparedQuery pq = datastore.prepare(query);
-
-        List<Item> items = new ArrayList<Item>();
-        List<Entity> entities = pq.asList(FetchOptions.Builder.withDefaults());
-        for (Entity entity : entities) {
-            items.add(entityToItem(entity));
-        }
-
         return items;
-    }
-
-    private Item entityToItem(Entity entity) {
-        Item item = new Item(
-                entity.getProperty("Name").toString(), entity.getProperty("Location").toString(),
-                Integer.parseInt(entity.getProperty("Price").toString()), Integer.parseInt(entity.getProperty("SellerId").toString()),
-                entity.getProperty("SellerName").toString(), entity.getProperty("Description").toString());
-        return item;
     }
 
 
