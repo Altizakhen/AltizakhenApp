af9ff61612f02a44bbb4ac3d0a84338e1a0d3ade
a7mad-3bbas <a7mad.3bass@gmail.com>
2015-01-18 23:22:18 +0200

    enable sort lists in home fragment by price

--- AltizakhenApplication/app/src/main/java/com/altizakhen/altizakhenapp/HomeFragment.java
+++ AltizakhenApplication/app/src/main/java/com/altizakhen/altizakhenapp/HomeFragment.java
@@ -8,13 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.altizakhen.altizakhenapp.backend.itemApi.model.Item;
 import com.altizakhen.altizakhenapp.itemsListAdapter.listAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by t-mansh on 12/29/2014.
@@ -38,6 +41,49 @@ public class HomeFragment extends Fragment{
         list = new ArrayList<Item>();
         listAdapter adapter = new listAdapter(getActivity(), MainActivity.items, 0, 1, itemList);
         itemList.setAdapter(adapter);
+
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.sort_spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sort_by_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter1);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (MainActivity.items == null) {
+                    return;
+                }
+                ArrayList<Item> sortedItems = new ArrayList<Item>(MainActivity.items);
+                if (i == 1) {
+
+                    for( int i1 = 0; i1 < sortedItems.size(); ++i1) {
+                        int max = i1;
+                        for(int j1 = i1; j1 < sortedItems.size(); ++j1) {
+                            if ( sortedItems.get(max).getPrice() < sortedItems.get(j1).getPrice()){
+                                max = j1;
+                            }
+                        }
+                        sortedItems.set(max,sortedItems.set(i1, sortedItems.get(max)));
+                    }
+                    MainActivity.items = sortedItems;
+                } else if (i==2) {
+
+                }
+                listAdapter adapter = new listAdapter(getActivity(), MainActivity.items, 0, 1, itemList);
+                itemList.setAdapter(adapter);
+                itemList.invalidateViews();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+
+            }
+        });
+
+
         /*Button b = (Button)rootView.findViewById(R.id.button3);
         b.setVisibility(View.INVISIBLE);*/
         itemList.setOnItemClickListener(new ListView.OnItemClickListener() {
